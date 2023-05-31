bbffcaeeab8b5ab8222ddc1c192b2aedd901cfc4
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-05-31 18:28:05 +0200

    hw1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import friends from 'data/friends.json';
 import FriendItem from './FriendItem';
+import PropTypes from 'prop-types';
 
 export default function FriendList() {
   return (
@@ -11,3 +12,12 @@ export default function FriendList() {
     </div>
   );
 }
+
+FriendList.propTypes = {
+  friends: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  }),
+};
